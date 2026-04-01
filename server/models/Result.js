58403317197a64d124bb96d97586fb14c9c3838c
sri nanveenrajr <srinaveenraj6@gmail.com>
2026-04-01 09:31:58 +0530

    14 th commit

--- server/models/Result.js
+++ server/models/Result.js
@@ -16,8 +16,24 @@ const resultSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    totalQuestions: {
+      type: Number,
+      default: 10,
+    },
+    timeTaken: {
+      type: Number, // in seconds
+    },
+    startTime: {
+      type: Date,
+    },
+    endTime: {
+      type: Date,
+    },
+    answers: {
+      type: Object, // Stores questionId: selectedOptionIndex
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Result", resultSchema);
